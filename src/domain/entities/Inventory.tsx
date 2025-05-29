56b6c7f3802778d2b369b47c065fd3083d52f35f
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-05-29 20:10:00 -0300

    ✨ Refactor Inventory class to use Farm object and update Item type definition

--- src/domain/entities/Inventory.tsx
+++ src/domain/entities/Inventory.tsx
@@ -1,20 +1,21 @@
+import Farm from './Farm';
+import Product from './Product';
+
 type Item = {
-  product_id: string;
+  product: Product;
   amount: number;
 }
 
 class Inventory {
   id: string;
-  farm_id: string;
-  farm_name: string;
+  farm: Farm;
   items: Item[];
   state: string;
   created_at: Date;
 
-  constructor(id: string, farm_id: string, farm_name: string, items: Item[], state: string, created_at: Date) {
+  constructor(id: string, farm: Farm, items: Item[], state: string, created_at: Date) {
     this.id = id;
-    this.farm_id = farm_id;
-    this.farm_name = farm_name;
+    this.farm = farm;
     this.items = items;
     this.state = state;
     this.created_at = created_at;
