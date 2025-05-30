0bd14e368637a0927ef717b9c7c988d0c3095ec6
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-05-30 19:25:39 -0300

    ✨ Add add method to InventoryRepository interface for adding inventory items

--- src/domain/repositories/InventoryRepository.ts
+++ src/domain/repositories/InventoryRepository.ts
@@ -1,5 +1,6 @@
 import Inventory from '@domain/entities/Inventory';
 
 export interface InventoryRepository {
+  add(inventory: Inventory): Promise<Inventory>;
   getAll(): Promise<Inventory[]>;
 };
